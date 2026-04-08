bbd61a761256ac80d5800ee66fc17c822ad798ae
minhvo2025 <minhwuo@gmail.com>
2026-04-08 20:52:36 +0200

    Update three-layer.js

--- js/three-layer.js
+++ js/three-layer.js
@@ -75,7 +75,7 @@ dummy: {
 },
   };
 
-const ARENA_MODEL_BASE_EULER = new THREE.Euler(0, 0, 0, 'XYZ');
+const ARENA_MODEL_BASE_EULER = new THREE.Euler(Math.PI, 0, 0, 'XYZ');
 
   function log(...args) {
     console.log('[Outra3D]', ...args);
